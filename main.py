0246c5675d9b16970b66fba2440dc655ca4cd613
Adria Blancafort <adriablancafort@gmail.com>
2024-08-15 13:58:57 +0200

    added CORS settings to allow local requests

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import product
 from database import create_db_and_tables
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(product.router)
